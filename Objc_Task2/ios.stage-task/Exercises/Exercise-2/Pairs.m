03096de0f8e249033cfe281d35817f51a91b040b
Vitali-Leikin <leikin1.19@gmail.com>
2021-05-16 18:53:27 +0300

    fix

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -5,20 +5,17 @@
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     int count = 0;
     
-    for (int i = 0; i < array.count - 1; i++) {
-        
-        for (int d = 0; d < array.count - 1 ; d++) {
-            
-            if(([array[i + 1] intValue] - [array[d] intValue]) == [number intValue]){
-                count += 1;
-                NSLog(@"%d", count);
-            }
-            
-        }
-        
-    }
-    NSLog(@"%d", count);
+    for (int i = 0; i < array.count; i++) {
 
+        for (int d = i + 1; d < array.count ; d++) {
+
+            if(ABS([array[i] intValue] - [array[d] intValue]) == [number intValue]){
+                count += 1;
+            }
+
+        }
+
+    }
     return count;
 }
 
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,48 +3,37 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    NSInteger counter = numbersArray.count;
-   NSMutableString *tempStr = [[NSMutableString alloc] init];
-    
-        if ([numbersArray  isEqual: @[]] || numbersArray == nil) {
-            return @"";
-        }else if (4 > numbersArray.count){
-            
-            for (NSInteger i = 0; i < numbersArray.count ; i++) {
-                
-                [tempStr appendFormat:@"%@", numbersArray[i]];
-            }
-            while (counter < 4) {
-                [tempStr appendString:@".0"];
-                counter +=1;
-                     }
-                return tempStr;
-        }else if(numbersArray.count > 4){
-            for (NSInteger i = 0; i < 4; i++) {
-                [ tempStr appendFormat:@"%@.",numbersArray[i] ];
-            }
-            return [tempStr substringToIndex:[tempStr length] - 1];
-        }
-    
-    
-    
-        
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+  
+
     for (NSInteger i = 0; i < numbersArray.count; i++) {
         if ([numbersArray[i] intValue] > 255){
             return  @"The numbers in the input array can be in the range from 0 to 255.";
         }else if (([numbersArray[i] intValue] < 0)){
             return @"Negative numbers are not valid for input.";
-        }else{
-            [ tempStr appendFormat:@"%@.",numbersArray[i] ];
-            
-            
         }
-        
-    }
-        
-        
+            }
+        switch (numbersArray.count) {
+            case 4:
+                four = [(NSNumber*)[numbersArray objectAtIndex:3] intValue];
+            case 3:
+                three = [(NSNumber*)[numbersArray objectAtIndex:2] intValue];
+            case 2:
+                two = [(NSNumber*)[numbersArray objectAtIndex:1] intValue];
+            case 1:
+                one = [(NSNumber*)[numbersArray objectAtIndex:0] intValue];
+                break;
+            default:
+                return @"";;
+        }
+    
+    NSString* str = [NSString stringWithFormat:@"%d.%d.%d.%d", one, two, three, four];
 
-    return [tempStr substringToIndex:[tempStr length] - 1]; ;
+       
+        return str;
 }
 
 @end
